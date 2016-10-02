81f7990146f7a666c4fb120eadb2372babbd4673
Koleok <kchamb3@gmail.com>
2016-10-02 17:48:41 -0400

    Util returns maybe

--- spec/convert-spec.js
+++ spec/convert-spec.js
@@ -3,15 +3,17 @@
 /* globals expect */
 /* eslint-disable no-template-curly-in-string */
 
+import S from 'sanctuary';
 import convert from '../lib/convertToTemplate';
 
 describe('convert when passed an empty string', () => {
   it('should return an empty string', () => {
-    expect(convert('')).toEqual('');
+    expect(convert('')).toEqual(S.Nothing());
   });
 
   it('should convert a concatenated string', () => {
-    expect(convert("'this ' + a + ' is the answer';"))
-      .toEqual('`this ${a} is the answer`;');
+    const str = "'this ' + a + ' is the answer';";
+    expect(convert(str))
+      .toEqual(S.Just('`this ${a} is the answer`;'));
   });
 });
